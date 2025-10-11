e3f5814eaba2515b7c284469defbccf1d111edbd
Huseyin Deniz <huseyin@depremapp.com>
2025-10-11 22:38:26 +0300

    Fix: Header import eklendi

--- app/src/main/java/com/example/depremapp/data/GeminiApiService.kt
+++ app/src/main/java/com/example/depremapp/data/GeminiApiService.kt
@@ -3,6 +3,7 @@ package com.example.depremapp.data
 import com.google.gson.annotations.SerializedName
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
